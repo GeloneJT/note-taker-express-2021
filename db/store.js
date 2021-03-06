359d141044e465578a9951fbf00fa059525ddfbf
Jelani Thomas <jelani13@icloud.com>
2021-03-06 02:50:57 -0500

    routing framework begun

---
+++ db/store.js
@@ -0,0 +1,26 @@
+const util = require("util");
+
+const uuidv1 = require("uuid");
+
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+
+class Store {
+    read(){
+        return readFileAsync('db/db.json', 'utf8')
+    }
+
+    write(note) {
+        return writeFileAsync('db/db.json', JSON.stringify(note));
+
+        //create a function to getNotes
+
+        //create a function to addNotes
+
+        //create a function to removeNotes by ID 
+
+    }
+}
+
+//export new store
+module.exports = new Store();
--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require('express');
 
+//require routes
+const notesRoute = require('./routes/notesRoute');
+const apiRoute = require('./routes/apiRoutes');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
+
+app.use('/', apiRoutes);
+app.use('/', notesRoute);
+
 
 app.listen(PORT, () => {
     console.log(`App listening on PORT: ${PORT}`);
