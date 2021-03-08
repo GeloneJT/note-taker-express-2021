293592f2386769079070fdd9540af4f92675b069
Jelani Thomas <jelani13@icloud.com>
2021-03-08 01:40:36 -0500

    added requests for apiRoutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -10,4 +10,22 @@ router.get('/notes', (req, res) =>{
     .catch((err) => res.status(500).json(err));
 })
 
+router.post('/notes', (req, res) =>{
+    store
+    .addNotes()
+    .then((notes) =>{
+        return res.json(notes);
+    })
+    .catch((err) => res.status(500).json(err));
+})
+
+router.delete('/notes', (req, res) =>{
+    store
+    .deleteNotes()
+    .then((notes) =>{
+        return res.json(notes);
+    })
+    .catch((err) => res.status(500).json(err));
+})
+
 module.exports = router;
